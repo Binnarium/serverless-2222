0f86c19069c75fe3efddb4b78e01a61e14cc716c
Bruno Esparza <bruno.be81@gmail.com>
2021-09-08 20:38:26 -0500

    add function when player was deleted from a chat

---
+++ functions/src/chats/services/remove-players-chat-id.ts
@@ -0,0 +1,21 @@
+import * as functions from "firebase-functions";
+import { UpdatePlayerGroupModel } from "../../players/models/update-player-group.model";
+import { FirestoreInstance } from "../../utils/configuration";
+import { ChatModel } from "../model/chat.model";
+
+export const removePlayersChatId = functions.firestore
+    .document('chats/{id}')
+    .onDelete(async (snapshot, context) => {
+        const batch = FirestoreInstance.batch();
+        const chat = <ChatModel>snapshot.data();
+
+        (chat.participantsUids as Array<string>).forEach(uid => {
+            // update player was removed from chat
+            const playerRef = FirestoreInstance.collection('players').doc(uid);
+            const playerUpdate: UpdatePlayerGroupModel = { addedToChat: false, chatId: null };
+
+            batch.update(playerRef, playerUpdate);
+        })
+
+        await batch.commit();
+    });
